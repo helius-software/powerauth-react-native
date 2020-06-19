a016ef32180ba1174a1a1be8672be3e3534499cc
Terri Prifti <terriprifti@gmail.com>
2020-06-19 12:09:42 +0200

    Validatepassword and unsafechange password methods

--- index.d.ts
+++ index.d.ts
@@ -5,27 +5,27 @@ declare module "react-native-powerauth" {
          The activation is just created.
         */
         PA2ActivationState_Created  = 1,
-        
+
         /**
          The OTP was already used.
         */
         PA2ActivationState_OTP_Used = 2,
-        
+
         /**
          The shared secure context is valid and active.
         */
         PA2ActivationState_Active   = 3,
-        
+
         /**
          The activation is blocked.
         */
         PA2ActivationState_Blocked  = 4,
-        
+
         /**
          The activation doesn't exist anymore.
         */
         PA2ActivationState_Removed  = 5,
-        
+
         /**
          The activation is technically blocked. You cannot use it anymore
         for the signature calculations.
@@ -43,10 +43,14 @@ declare module "react-native-powerauth" {
     export function createActivation(credentials: any): Promise<string>;
 
     export function commitActivation(password: string): Promise<string>;
-    
+
     export function removeActivationLocal(): void;
-    
+
     export function hasValidActivation(): Promise<boolean>;
-    
+
     export function fetchActivationStatus(): Promise<ActivationStatus>;
+
+    export function validatePasswordCorrect(oldPassword: string): Promise<string>;
+
+    export function unsafeChangePassword(oldPassword: string, newPassword: string): Promise<string>;
 }
--- ios/PowerAuth.m
+++ ios/PowerAuth.m
@@ -24,133 +24,158 @@
     RCT_EXPORT_MODULE();
 
 
-  RCT_EXPORT_METHOD(createActivation:
+    RCT_EXPORT_METHOD(createActivation:
                   (NSDictionary*) credentials
                   createActivationResolver: (RCTPromiseResolveBlock) resolve
                   createActivationRejecter: (RCTPromiseRejectBlock) reject) {
 
-     NSString* deviceName = [[UIDevice currentDevice] name];
+        NSString* deviceName = [[UIDevice currentDevice] name];
 
-    [[PowerAuthSDK sharedInstance] createActivationWithName: deviceName identityAttributes:credentials extras: nil callback:^(PA2ActivationResult * _Nullable result, NSError * _Nullable error) {
+        [[PowerAuthSDK sharedInstance] createActivationWithName: deviceName identityAttributes:credentials extras: nil callback:^(PA2ActivationResult * _Nullable result, NSError * _Nullable error) {
 
-         if(error == nil) {
-           NSString *successMessage = @"OK";
+            if(error == nil) {
+                NSString *successMessage = @"OK";
 
-           resolve(successMessage);
-         } else {
-           reject(@"Error", error.localizedDescription, nil);
-         }
-     }];
-  }
+                resolve(successMessage);
+            } else {
+                reject(@"Error", error.localizedDescription, nil);
+            }
+        }];
+    }
 
-  RCT_EXPORT_METHOD(commitActivation:
+    RCT_EXPORT_METHOD(commitActivation:
                     (NSString*) password
                     commitActivationResolver: (RCTPromiseResolveBlock) resolve
                     commitActivationRejecter: (RCTPromiseRejectBlock) reject) {
 
-    NSError* errorMessage = [NSError errorWithDomain:@"com.heliussystems.PowerauthReact" code:200 userInfo:@{NSLocalizedDescriptionKey: @"Commit activation was completed successfully."}];
+        NSError* errorMessage = [NSError errorWithDomain:@"com.heliussystems.PowerauthReact" code:200 userInfo:@{NSLocalizedDescriptionKey: @"Commit activation was completed successfully."}];
 
-    bool success = [[PowerAuthSDK sharedInstance] commitActivationWithPassword:password error: &errorMessage];
+        bool success = [[PowerAuthSDK sharedInstance] commitActivationWithPassword:password error: &errorMessage];
 
-    if(success) {
-      resolve(@"SUCCESS");
-    } else {
-      reject(@"error", errorMessage.localizedDescription, nil);
+        if(success) {
+            resolve(@"SUCCESS");
+        } else {
+            reject(@"error", errorMessage.localizedDescription, nil);
+        }
     }
 
-  }
-
-  RCT_EXPORT_METHOD(removeActivationLocal) {
-    NSLog(@"REMOVED ACTIVATION");
-    [[PowerAuthSDK sharedInstance] removeActivationLocal];
-  }
-
-  RCT_REMAP_METHOD(hasValidActivation,
-                    hasValidActivationResolver: (RCTPromiseResolveBlock) resolve
-                    hasValidActivationRejecter: (RCTPromiseRejectBlock) reject) {
-
-    bool validActivation = [[PowerAuthSDK sharedInstance] hasValidActivation];
-
-    if(validActivation) {
-      resolve(@YES);
-    } else {
-      reject(@"400", @"false", nil);
+    RCT_EXPORT_METHOD(removeActivationLocal) {
+        NSLog(@"REMOVED ACTIVATION");
+        [[PowerAuthSDK sharedInstance] removeActivationLocal];
     }
-  }
 
-RCT_REMAP_METHOD(fetchActivationStatus,
+    RCT_REMAP_METHOD(hasValidActivation,
+                        hasValidActivationResolver: (RCTPromiseResolveBlock) resolve
+                        hasValidActivationRejecter: (RCTPromiseRejectBlock) reject) {
+
+        bool validActivation = [[PowerAuthSDK sharedInstance] hasValidActivation];
+
+        if(validActivation) {
+            resolve(@YES);
+        } else {
+            reject(@"400", @"false", nil);
+        }
+    }
+
+    RCT_REMAP_METHOD(fetchActivationStatus,
                 fetchActivationStatusResolver: (RCTPromiseResolveBlock) resolve
                 fetchActivationStatusRejecter: (RCTPromiseRejectBlock) reject) {
 
-  bool validActivation = [[PowerAuthSDK sharedInstance] hasValidActivation];
+        bool validActivation = [[PowerAuthSDK sharedInstance] hasValidActivation];
 
-  NSLog(@"VALID ACTIVATION STATUS: %d\n", validActivation);
+        NSLog(@"VALID ACTIVATION STATUS: %d\n", validActivation);
 
-  if(!validActivation) {
-    reject(@"400", @"Activation is invalid", nil);
-    return;
-  }
+        if(!validActivation) {
+            reject(@"400", @"Activation is invalid", nil);
+            return;
+        }
 
-  [[PowerAuthSDK sharedInstance] fetchActivationStatusWithCallback:^(PA2ActivationStatus * _Nullable status, NSDictionary * _Nullable customObject, NSError * _Nullable error) {
+        [[PowerAuthSDK sharedInstance] fetchActivationStatusWithCallback:^(PA2ActivationStatus * _Nullable status, NSDictionary * _Nullable customObject, NSError * _Nullable error) {
 
-    if(error == nil) {
-      PA2ActivationState state = status.state;
+            if(error == nil) {
+                PA2ActivationState state = status.state;
 
-      int currentFailCount = status.failCount;
-      int maxAllowedFailCount = status.maxFailCount;
-      int remainingFailCount = maxAllowedFailCount - currentFailCount;
+                int currentFailCount = status.failCount;
+                int maxAllowedFailCount = status.maxFailCount;
+                int remainingFailCount = maxAllowedFailCount - currentFailCount;
 
-      NSDictionary *response = @{
-        @"status": @(state),
-        @"currentFailCount": @(currentFailCount),
-        @"maxAllowedFailCount": @(maxAllowedFailCount),
-        @"remainingFailCount" : @(remainingFailCount)
-      };
+                NSDictionary *response = @{
+                    @"status": @(state),
+                    @"currentFailCount": @(currentFailCount),
+                    @"maxAllowedFailCount": @(maxAllowedFailCount),
+                    @"remainingFailCount" : @(remainingFailCount)
+                };
 
-      resolve(response);
+                resolve(response);
 
-
-    } else {
-      // network error occured, report it to the user
-
-      reject(@"400", @"Fetch activation status failed", nil);
-
+            } else {
+                // network error occured, report it to the user
+                reject(@"400", @"Fetch activation status failed", nil);
+            }
+        }];
     }
-  }];
-}
 
-RCT_EXPORT_METHOD(requestSignature:
-                  (NSString*) userPassword
-                  withRequestmethod: (NSString*) requestMethod
-                          withUriId: (NSString*) uriId
-                    withRequestData: (NSDictionary*) reqData
-                  requestSignatureResolver : (RCTPromiseResolveBlock) resolve
-                  requestSignature : (RCTPromiseRejectBlock) reject) {
+    RCT_EXPORT_METHOD(requestSignature:
+                    (NSString*) userPassword
+                    withRequestmethod: (NSString*) requestMethod
+                            withUriId: (NSString*) uriId
+                        withRequestData: (NSDictionary*) reqData
+                    requestSignatureResolver : (RCTPromiseResolveBlock) resolve
+                    requestSignature : (RCTPromiseRejectBlock) reject) {
 
-  PowerAuthAuthentication *auth = [[PowerAuthAuthentication alloc] init];
-  auth.usePossession = true;
-  auth.usePassword = userPassword;
-  auth.useBiometry = false;
+        PowerAuthAuthentication *auth = [[PowerAuthAuthentication alloc] init];
+        auth.usePossession = true;
+        auth.usePassword = userPassword;
+        auth.useBiometry = false;
 
-  NSData* requestData = [NSKeyedArchiver archivedDataWithRootObject:reqData];
+        NSData* requestData = [NSKeyedArchiver archivedDataWithRootObject:reqData];
 
-  NSLog(@"Request data log: %@", requestData);
+        NSLog(@"Request data log: %@", requestData);
 
-  @try {
-    NSError* errorMessage = [NSError errorWithDomain:@"com.heliussystems.eposta" code:200 userInfo:@{NSLocalizedDescriptionKey: @""}];
+        @try {
+            NSError* errorMessage = [NSError errorWithDomain:@"com.heliussystems.eposta" code:200 userInfo:@{NSLocalizedDescriptionKey: @""}];
 
-   PA2AuthorizationHttpHeader* signature = [[PowerAuthSDK sharedInstance] requestSignatureWithAuthentication:auth method:requestMethod uriId:uriId body:requestData error: &errorMessage];
+            PA2AuthorizationHttpHeader* signature = [[PowerAuthSDK sharedInstance] requestSignatureWithAuthentication:auth method:requestMethod uriId:uriId body:requestData error: &errorMessage];
 
-    NSDictionary *response = @{
-      @"httpHeaderKey": signature.key,
-      @"httpHeaderValue": signature.value
-    };
+            NSDictionary *response = @{
+                @"httpHeaderKey": signature.key,
+                @"httpHeaderValue": signature.value
+            };
 
-    return resolve(response);
+            return resolve(response);
 
-  } @catch (NSException *exception) {
-    return reject(@"Error", exception.reason, nil);
-  }
-}
+        } @catch (NSException *exception) {
+            return reject(@"Error", exception.reason, nil);
+        }
+    }
+
+    RCT_EXPORT_METHOD(validatePasswordCorrect:
+                    (NSString*) oldPassword
+                    validatePasswordCorrectResolver : (RCTPromiseResolveBlock) resolve
+                    validatePasswordCorrectRejecter : (RCTPromiseRejectBlock) reject) {
+
+        [[PowerAuthSDK sharedInstance] validatePasswordCorrect:oldPassword callback:^(NSError * _Nullable error) {
+            if(error == nil) {
+                NSString* successMessage = @"valid";
+
+                resolve(successMessage);
+            } else {
+                reject(@"Error", error.localizedDescription, nil);
+            }
+        }];
+    }
+
+    RCT_EXPORT_METHOD(unsafeChangePassword:
+                    (NSString*) oldPassword
+                    withNewPassword: (NSString*) newPassword
+                    unsafeChangePasswordResolver : (RCTPromiseResolveBlock) resolve
+                    unsafeChangePasswordRejecter : (RCTPromiseRejectBlock) reject) {
+
+        [[PowerAuthSDK sharedInstance] unsafeChangePasswordFrom: oldPassword to:newPassword];
+
+        NSString* successMessage = @"password_changed";
+
+        resolve(successMessage);
+    }
 
 @end
